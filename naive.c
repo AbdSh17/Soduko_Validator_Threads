4afb9938424356d8f25042d25d1d1f7ef652dcee
AbdSh17 <abdalraheem200417a@gmail.com>
2025-04-26 02:58:51 +0300

    add print to know whhere's the error exactly

--- naive.c
+++ naive.c
@@ -20,14 +20,14 @@ bool validateSoduko();
 // ======================== Defines ========================
 
 int sudoku[9][9] = {
-    {8, 2, 7, 1, 5, 4, 3, 9, 6},
+    {1, 2, 7, 1, 5, 4, 3, 9, 6},
     {9, 6, 5, 3, 2, 7, 1, 4, 8},
     {3, 4, 1, 6, 8, 9, 7, 5, 2},
     {5, 9, 3, 4, 6, 8, 2, 7, 1},
     {4, 7, 2, 5, 1, 3, 6, 8, 9},
     {6, 1, 8, 9, 7, 2, 4, 3, 5},
     {7, 8, 6, 2, 3, 5, 9, 1, 4},
-    {1, 5, 4, 7, 9, 6, 8, 2, 3},
+    {8, 5, 4, 7, 9, 6, 8, 2, 3},
     {2, 3, 9, 8, 4, 1, 5, 6, 7}};
 
 int main()
@@ -54,6 +54,7 @@ bool validateRow()
         }
         if (count != 45)
         {
+            printf("Error with Row: %d\n", i);
             return false;
         }
     }
@@ -72,6 +73,7 @@ bool validateColumn()
         }
         if (count != 45)
         {
+            printf("Error with Column: %d\n", i);
             return false;
         }
     }
@@ -86,6 +88,7 @@ bool validateAllSubgrids()
         {
             if (!validateSubgrid(i, j))
             {
+                printf("Error with Subgrid: (%d,%d)", i, j);
                 return false;
             }
         }
